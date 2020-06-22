06c317576c2bdebfedbd57afe0a082539b93c5bb
Tomodo531 <47330347+Tomodo531@users.noreply.github.com>
2020-06-22 14:59:07 +0200

    Updated

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -5,6 +5,12 @@ let Post = require('../models/post.model');
 const verify = require('./verifyToken');
 var AWS = require('aws-sdk');
 
+const imagemin = require('imagemin');
+const mozjpeg = require('imagemin-mozjpeg');
+const sharp = require('sharp');
+var sizeOf = require('image-size');
+const isJpg = require('is-jpg');
+
 const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID;
 const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY;
 const BUCKET = process.env.BUCKET;
@@ -14,8 +20,35 @@ router.get('/', (req, res) => {
 	Post.find().sort({ pined: -1, createdAt: -1 }).then((posts) => res.json(posts)).catch((err) => res.status(400));
 });
 
+async function imageCompression(file) {
+	const convertToJpg = async (input) => {
+		const inputDimensions = sizeOf(input);
+		const largestDimension =
+			inputDimensions.width > inputDimensions.height
+				? { width: inputDimensions.width > 1000 ? 1000 : inputDimensions.wdith }
+				: { height: inputDimensions.height > 1000 ? 1000 : inputDimensions.height };
+
+		if (isJpg(input)) {
+			return sharp(input).resize(largestDimension).toBuffer();
+		}
+
+		return sharp(input).resize(largestDimension).jpeg().toBuffer();
+	};
+
+	if (!file.media.mimetype.includes('gif') && !file.media.mimetype.includes('mp4')) {
+		const compressedFile = await imagemin.buffer(file.media.data, {
+			plugins: [ convertToJpg, mozjpeg({ quality: 85 }) ]
+		});
+
+		return compressedFile;
+	} else {
+		console.log('file: ', file.media.data);
+		return file.media.data;
+	}
+}
+
 //POST: '/posts/add'
-router.post('/add', verify, (req, res) => {
+router.post('/add', verify, async (req, res) => {
 	var post = new Post({
 		media: null,
 		content: req.body.content
@@ -36,7 +69,7 @@ router.post('/add', verify, (req, res) => {
 			return res.status(400).json('File type not supported').send();
 		}
 
-		post.media = req.files.media.name = uuidv4() + '.' + req.files.media.mimetype.split('/')[1];
+		post.media = req.files.media.name = uuidv4();
 
 		const s3 = new AWS.S3({
 			accessKeyId: ACCESS_KEY_ID,
@@ -47,7 +80,7 @@ router.post('/add', verify, (req, res) => {
 		var params = {
 			Bucket: BUCKET,
 			Key: post.media, // File name you want to save as in S3
-			Body: req.files.media.data
+			Body: await imageCompression(req.files)
 		};
 
 		var awsUploadPromise = new Promise((resolve, reject) => {
@@ -79,12 +112,10 @@ router.post('/pin/:id', verify, (req, res) => {
 		.then((post) => {
 			post.pined = !post.pined;
 
-			post.save()
-			.then(() => res.status(200).send('Post pined'))
-			.catch((err) => res.status(400).send(err))
+			post.save().then(() => res.status(200).send('Post pined')).catch((err) => res.status(400).send(err));
 		})
 		.catch((err) => res.status(400).send(err));
-})
+});
 
 router.post('/remove/:id', verify, (req, res) => {
 	Post.findById(req.params.id)
